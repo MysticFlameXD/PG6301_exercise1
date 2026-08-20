d5f836c58c042a6ae5fc11d87ff62b4ece0d6d71
MysticFlameXD <mysticflamealt@gmail.com>
2026-08-20 15:41:23 +0200

    step 6

--- romanNumerals.test.js
+++ romanNumerals.test.js
@@ -1,8 +1,12 @@
 import { test, expect } from "vitest";
 
 function romanNumerals(number) {
-  if (number === 1) return "I";
-  if (number === 2) return "II";
+  let romanNumber = "";
+  for (let i = 0; i < number; i++) {
+    romanNumber += "I";
+  }
+
+  return romanNumber;
 }
 
 test("1 in roman numerals is I", () => {
@@ -12,5 +16,8 @@ test("2 in roman numerals is II", () => {
   expect(romanNumerals(2)).toBe("II");
 });
 test("3 in roman numerals is III", () => {
-  expect(romanNumerals(2)).toBe("III");
+  expect(romanNumerals(3)).toBe("III");
+});
+test("4 in roman numerals is IV", () => {
+  expect(romanNumerals(4)).toBe("IV");
 });
