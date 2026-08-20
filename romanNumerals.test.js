e617fab3a330a1beb78c1c991a8a0244f6c25647
MysticFlameXD <mysticflamealt@gmail.com>
2026-08-20 15:33:53 +0200

    step 4

--- romanNumerals.test.js
+++ romanNumerals.test.js
@@ -1,5 +1,12 @@
 import { test, expect } from "vitest";
 
+function romanNumerals(number) {
+  return "I";
+}
+
 test("1 in roman numerals is I", () => {
   expect(romanNumerals(1)).toBe("I");
+});
+test("2 in roman numerals is II", () => {
+  expect(romanNumerals(2)).toBe("II");
 });
